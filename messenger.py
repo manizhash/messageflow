28b915c03f43113f082ba4724e9b546d55ac47db
manizhash <msharipova065@gmail.com>
2024-04-22 21:50:13 +0500

    write functions

--- messenger.py
+++ messenger.py
@@ -18,13 +18,47 @@ class Messenger(QtWidgets.QMainWindow, Ui_MainWindow):
         self.timer.start(1000)
 
     def print_message(self, message):
-        pass
-    
+        dt = datetime.fromtimestamp(message["time"])
+        str_dt = dt.strftime("%d %b %H:%M:%S")
+        self.textBrowser.append(str_dt + " " + message["name"])
+        self.textBrowser.append(message["text"])
+        self.textBrowser.append(" ")
+
     def get_messages(self):
-        pass
+        try:
+            response = requests.get(
+                "http://127.0.0.1:5000/messages",
+                params={"after": self.after}
+            )
+        except:
+            return
+
+        messages = response.json()["messages"]
+        for message in messages:
+            self.print_message(message)
+            self.after = message["time"]
 
     def send_message(self):
-        pass
+        name = self.lineEdit.text()
+        text = self.plainTextEdit.toPlainText()
+
+        try:
+            response = requests.post(
+                "http://127.0.0.1:5000/send",
+                json={"name": name, "text": text}
+            )
+        except:
+            self.textBrowser.append("Сервер недоступен")
+            self.textBrowser.append("Попробуйте еще раз")
+            self.textBrowser.append(" ")
+            return
+
+        if response.status_code != 200:
+            self.textBrowser.append("Имя и текст не должны быть пустыми. Текст <= 1000 символов.")
+            self.textBrowser.append(" ")
+            return
+
+        self.plainTextEdit.clear()
 
 
 app = QtWidgets.QApplication([])
