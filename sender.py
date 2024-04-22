dd075214a01f15c57813b8a0bb3b9c7c6f24067d
manizhash <msharipova065@gmail.com>
2024-04-22 21:52:31 +0500

    write sender func

--- sender.py
+++ sender.py
@@ -1,4 +1,10 @@
+import requests
+
 name = input("Введите Имя: ")
 
 while True:
-    pass
+    text = input()
+    response = requests.post(
+        "http://127.0.0.1:5000/send",
+        json={"name": name, "text": text}
+    )
